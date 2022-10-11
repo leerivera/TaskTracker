d4ec189e39e2dee50664f69527bd3e4d46f4bdb2
leerivera <leerivera80@gmail.com>
2022-10-11 14:38:41 -0400

    tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,32 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
-const tasks = [
-    {
-        id:1,
-        text: 'Duolingo',
-        day: 'Oct 11th at 9:00am',
-        reminder: true
-    },
-    {
-        id:2,
-        text: "Write bits",
-        day: 'Oct 11th at 11:00am',
-        reminder: true,
-
-    },
-    {
-        id:3,
-        text: "Exercise",
-        day: 'Oct 11th at 12:00pm',
-        reminder: false,
-    }
-]
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState(    [{
+    id:1,
+    text: 'Duolingo',
+    day: 'Oct 11th at 9:00am',
+    reminder: true
+},
+{
+    id:2,
+    text: "Write bits",
+    day: 'Oct 11th at 11:00am',
+    reminder: true,
+
+},
+{
+    id:3,
+    text: "Exercise",
+    day: 'Oct 11th at 12:00pm',
+    reminder: false,
+},
+])
   return (
     <>
       {tasks.map((task) => (
-          <h3>{task.text}</h3>
+          <h3 key={task.id}>{task.text}</h3>
       ))}
     </>
   )
